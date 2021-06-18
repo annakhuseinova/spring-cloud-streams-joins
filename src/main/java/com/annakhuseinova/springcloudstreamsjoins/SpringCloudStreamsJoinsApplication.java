9a558d30460d210ad481674c39a6385053017812
annakhuseinova <annakhuseinova@yandex.ru>
2021-06-18 15:47:48 +0300

    Initial commit

---
+++ src/main/java/com/annakhuseinova/springcloudstreamsjoins/SpringCloudStreamsJoinsApplication.java
@@ -0,0 +1,13 @@
+package com.annakhuseinova.springcloudstreamsjoins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamsJoinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudStreamsJoinsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/annakhuseinova/springcloudstreamsjoins/SpringCloudStreamsJoinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annakhuseinova.springcloudstreamsjoins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStreamsJoinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
